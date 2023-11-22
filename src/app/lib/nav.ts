dde2a9350f05e7daabaf073124a888bf2b750c93
Tomas Paoletti Velado <tomas.paolettiv@hotmail.com>
2023-11-22 21:05:46 +0100

    create app route and styled home page

---
+++ src/app/lib/nav.ts
@@ -0,0 +1,15 @@
+export const nav = [
+    {
+      "id": 1,
+     "name": "Experiencia",
+     "route": "experience" 
+    },{
+      "id": 2,
+      "name": "Proyectos",
+      "route": "proyects"
+    },{
+      "id": 3,
+      "name": "Contacto",
+      "route": "contact"
+    }
+  ]
